17a5f9153e6aba87668de587eca5d6d0c0aee0bd
pythagoras58 <solomondanso58@gmail.com>
2022-07-17 00:06:20 +0000

    Add project files.

---
+++ Csharp/Circle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp
+{
+    class Circle
+    {
+        // variables
+        double radius;
+        double pi = 3.14;
+
+        public void AcceptRadius()
+        {
+            radius = 4.5;
+        }
+
+        public double GetArea()
+        {
+            return pi * radius * radius;
+        }
+
+        public void showResults()
+        {
+            Console.WriteLine("Radius: {0}", radius);
+            Console.WriteLine("Areaa: {1}", GetArea());
+        }
+    }
+}
